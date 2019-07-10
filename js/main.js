a6eafafc155b4333a0a5862a56f4b243bf07eedc
Timothy Do <47342411+TimTheCoder123@users.noreply.github.com>
2019-07-10 00:34:13 -0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -113,8 +113,8 @@ function gotStream(stream) {
 }
 
 var constraints = {
-  video: true
-  audio: true
+  video: true,
+  audio: true,
 };
 
 console.log('Getting user media with constraints', constraints);
