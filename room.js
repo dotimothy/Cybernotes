e33c50bf01dd22ad14ce097800f752078744f6bb
Timothy Do <47342411+TimTheCoder123@users.noreply.github.com>
2019-07-18 23:24:33 -0700

    Changed some

--- room.js
+++ room.js
@@ -1,3 +1,14 @@
-function enterRoom {
-	window.room = prompt("Enter room name:");
+function enterRoom() {
+	var name = prompt("Enter room name:");
+	var password = prompt("Enter password");	
+	if(name = "euphonium" && password == "euphonium") {
+		window.location.href='demo.html';
+	}
+	else if(input != "") {
+		alert("Wrong room name/password. Please try again");
+		enterRoom();
+	}
+	else {
+		return;
+	}
 }
