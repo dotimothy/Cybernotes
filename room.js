5e72a0f02548b0ee2e36f1d4f6ea20f2cad2f637
Timothy Do <47342411+TimTheCoder123@users.noreply.github.com>
2019-07-19 23:31:46 -0700

    Deleted Demo

--- room.js
+++ room.js
@@ -1,12 +1,14 @@
 function enterRoom() {
 	var name = prompt("Enter room name:");
-	var password = prompt("Enter password");	
+	var password = prompt("Enter password");
+	var login = document.getElementById('login');	
+	var demo = document.getElementById('demo');
 	if(name = "euphonium" && password == "euphonium") {
-		window.location.href='demo.html';
+		login.setAttribute("style","display:none");
+		demo.setAttribute("style","display:block");
 	}
 	else if(input != "") {
 		alert("Wrong room name/password. Please try again");
-		enterRoom();
 	}
 	else {
 		return;
